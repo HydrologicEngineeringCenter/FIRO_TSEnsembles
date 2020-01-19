5e3a1c109032ab63a80de77c9e78e0fe079d38a0
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-01-18 21:32:02 -0800

    add unit test to understand/test TreeMap.floorKey with ZonedDateTime

--- src/test/java/hec/ensemble/Testing.java
+++ src/test/java/hec/ensemble/Testing.java
@@ -8,6 +8,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -268,5 +269,51 @@ public class Testing {
 
     private Object ProcessEnsemble(Ensemble e) {
         return new Object();
+    }
+
+    /**
+     * Test  TreeMap.floorKey with Key of ZonedDateTime
+     * @throws Exception
+     */
+    @Test
+    public void testTimeTolerance()throws Exception
+    {
+        TreeMap<ZonedDateTime,Integer> items = new TreeMap<ZonedDateTime, Integer>();
+        ZonedDateTime t = ZonedDateTime.of(2013, 11, 1, 12, 0, 0, 0, ZoneId.of("GMT"));
+
+        ZonedDateTime nov1 = t;
+        ZonedDateTime nov3 = t.plusDays(2);
+        ZonedDateTime nov5 = t.plusDays(4);
+        ZonedDateTime nov10 = t.plusDays(9);
+        ZonedDateTime nov15 = t.plusDays(14);
+
+        items.put(nov3,3);
+        items.put(nov5,5);
+        items.put(nov15,15);
+
+        t =  items.floorKey(nov3);
+
+        // lookup existing nov 3
+        int toleranceHours = 24*5;
+        int c =  t.plusHours(toleranceHours).compareTo(nov3);
+        assertEquals(1,c);
+
+        // lookup missing nov 1 (before beginning)
+        t =  items.floorKey(nov1);
+        assertNull(t);
+
+        // lookup nov10 without enough tolerance
+        toleranceHours=4;
+        t = items.floorKey(nov10);
+        c =  t.plusHours(toleranceHours).compareTo(nov10);
+        assertEquals(-1,c);
+
+        // lookup nov10 with just enough tolerance
+        toleranceHours=24*5;
+        t = items.floorKey(nov10);
+        c =  t.plusHours(toleranceHours).compareTo(nov10);
+        assertEquals(0,c);
+
+
     }
 }
