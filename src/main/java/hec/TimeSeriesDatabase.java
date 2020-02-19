70eca6261ed688843ce5bf87501aee1b3ed1e512
Mike Neilson <michael.a.neilson@usace.army.mil>
2020-02-19 13:14:48 -0800

    Cleaned up some formatting.

--- src/main/java/hec/TimeSeriesDatabase.java
+++ src/main/java/hec/TimeSeriesDatabase.java
@@ -7,19 +7,14 @@ import hec.paireddata.*;
 
 public abstract class TimeSeriesDatabase {
 
-	public abstract Ensemble getEnsemble(TimeSeriesIdentifier timeseriesID, ZonedDateTime issue_time);    
+    public abstract Ensemble getEnsemble(TimeSeriesIdentifier timeseriesID, ZonedDateTime issue_time);    
     public abstract EnsembleTimeSeries getEnsembleTimeSeries(TimeSeriesIdentifier timeseriesID);
     public abstract PairedData getPairedData(String string);
     public abstract int getCount(TimeSeriesIdentifier timeseriesID);
     public abstract List<ZonedDateTime> getIssueDates(TimeSeriesIdentifier timeseriesID);
-
     public abstract void write(EnsembleTimeSeries[] etsArray) throws Exception;
     public abstract void write(EnsembleTimeSeries ets) throws Exception;	
-	public abstract void write(PairedData table);
-	
-	
-
-
+    public abstract void write(PairedData table);
     public abstract void write(EnsembleTimeSeries ets) throws Exception;
     public abstract TimeSeriesIdentifier[] getTimeSeriesIDs();
 
