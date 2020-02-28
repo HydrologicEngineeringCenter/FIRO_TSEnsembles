a9412b16c7e2772838a402304be276ba2734c910
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-02-28 13:12:34 -0800

    refactor to provide read-modify-write metrics

---
+++ src/main/java/hec/ensemble/EnsembleUtility.java
@@ -0,0 +1,126 @@
+package hec.ensemble;
+
+import hec.JdbcTimeSeriesDatabase;
+import hec.TimeSeriesDatabase;
+
+import java.io.File;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Creates an ensemble test database for use with ResSim scripting
+ *
+ */
+public class EnsembleUtility {
+
+    public static void main(String[] args)throws Exception {
+        if( args.length != 4)
+        {
+            System.out.println("EnsembleUtility hefs_cache_dir numDays numLocations output.db");
+            System.out.println("Example:  ");
+            System.out.println("EnsembleUtility c:/temp/hefs_cache 30 1 ResSim.db");
+            return;
+        }
+        int numDays = Integer.parseInt(args[1].trim());
+        int numLocations = Integer.parseInt(args[2].trim());
+
+        createTestDatabase(args[0],numDays, numLocations,args[3]);
+        readModifyWrite(args[3]);
+
+    }
+
+    /**
+     * Read some data from 2015,  re-label as starting in 1996 for testing ResSim
+     * @param hefs_dir
+     * @param filename
+     * @return
+     * @throws Exception
+     */
+    static private TimeSeriesDatabase createTestDatabase(String hefs_dir,int numberOfDates, int numLocations,
+                                                         String filename) throws Exception {
+
+        File f = new File(filename);
+        if( f.exists())
+           f.delete();
+        ZonedDateTime issueDate1 = ZonedDateTime.of(2015, 11, 3, 12, 0, 0, 0, ZoneId.of("GMT"));
+        ZonedDateTime issueDate2 = issueDate1.plusDays(numberOfDates);
+
+        CsvEnsembleReader csvReader = new CsvEnsembleReader(hefs_dir);
+        EnsembleTimeSeries[] ets = csvReader.Read("RussianNapa", issueDate1, issueDate2);
+        TimeSeriesDatabase db = new JdbcTimeSeriesDatabase(filename, JdbcTimeSeriesDatabase.CREATION_MODE.CREATE_NEW);
+        ZonedDateTime startDateTime = ZonedDateTime.of(1996, 12, 24, 12, 0, 0, 0, ZoneId.of("GMT"));
+
+        // modify start/issue dates/ location
+        EnsembleTimeSeries etsOut = null;
+        List<EnsembleTimeSeries> outList = new ArrayList<EnsembleTimeSeries>();
+        for (int i = 0; i <Math.min(ets.length,numLocations) ; i++) {
+            EnsembleTimeSeries a = ets[i];
+            TimeSeriesIdentifier tsid = a.getTimeSeriesIdentifier();
+            if( i == 0) {
+                tsid = new TimeSeriesIdentifier("Coyote.fake_forecast","flow");
+            }
+            List<ZonedDateTime> dates = a.getIssueDates();
+
+            etsOut = new EnsembleTimeSeries(tsid, a.getUnits(), a.getDataType(), a.getVersion());
+            ZonedDateTime newIssueDate = startDateTime;
+            for (int j = 0; j < a.getCount(); j++) {
+                Ensemble e = a.getEnsemble(dates.get(j));
+                Ensemble e2 = new Ensemble(newIssueDate, e.getValues(), newIssueDate, e.getInterval());
+                newIssueDate = newIssueDate.plusDays(1);
+                etsOut.addEnsemble(e2);
+            }
+            outList.add(etsOut);
+        }
+        db.write(outList.toArray(new EnsembleTimeSeries[0]));
+        return db;
+    }
+
+    /**
+     * Reads an ensemble database into memory,
+     * modifies ensemble data, then writes to a temporary file
+     * logs time for reading and writing.
+     *
+     * @param fileName
+     * @throws Exception
+     */
+    static void readModifyWrite(String fileName) throws Exception {
+        long startTime = System.nanoTime();
+        TimeSeriesDatabase db = new JdbcTimeSeriesDatabase(fileName, JdbcTimeSeriesDatabase.CREATION_MODE.OPEN_EXISTING_UPDATE);
+        List<TimeSeriesIdentifier> locations = db.getTimeSeriesIDs();
+
+        ArrayList<EnsembleTimeSeries> etsList = new ArrayList<>();
+        int count = 0;
+        for(TimeSeriesIdentifier tsid: locations) {
+            System.out.println(tsid.toString());
+            EnsembleTimeSeries ets = db.getEnsembleTimeSeries(tsid);
+            for (ZonedDateTime t: ets.getIssueDates()){
+                Ensemble e = ets.getEnsemble(t);
+                for(float[] v : e.getValues()){
+                    for (int i = 0; i <v.length ; i++) {
+                        v[i] = v[i]+10 ;// offset by 10
+                    }
+                }
+            }
+            etsList.add(ets);
+            count += ets.getCount();
+        }
+
+        long endTime = System.nanoTime();
+        double seconds = (endTime - startTime)/1000000.0/1000.;
+        System.out.println("Time To Read/Modify : "+seconds+" s");
+        File f = File.createTempFile("delete_me",".sqlite");
+        String outputPath = f.getName();
+        f.delete();
+
+        startTime = System.nanoTime();
+        JdbcTimeSeriesDatabase dbaseOut = new JdbcTimeSeriesDatabase(outputPath, JdbcTimeSeriesDatabase.CREATION_MODE.CREATE_NEW_OR_OPEN_EXISTING_UPDATE);
+        dbaseOut.write(etsList.toArray(new EnsembleTimeSeries[0]));
+        endTime = System.nanoTime();
+        seconds = (endTime - startTime)/1000000.0/1000.0;
+        System.out.println("Write back to disk : "+seconds+" s");
+
+        // f.delete();
+    }
+}
