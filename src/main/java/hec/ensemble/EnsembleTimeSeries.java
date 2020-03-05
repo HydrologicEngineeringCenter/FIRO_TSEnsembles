2f043ace1a5497b9ca8d1a5571c8dff679936d1f
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2020-03-05 07:25:16 -0800

    delete IEnsembleTimeSeries.java

--- src/main/java/hec/ensemble/EnsembleTimeSeries.java
+++ src/main/java/hec/ensemble/EnsembleTimeSeries.java
@@ -11,7 +11,7 @@ import java.util.TreeMap;
  *  EnsembleTimeSeries is a collection of Ensembles over time
  *
  */
-public class EnsembleTimeSeries implements  Iterable<Ensemble>,IEnsembleTimeSeries
+public class EnsembleTimeSeries implements  Iterable<Ensemble> 
   {
 
     private TimeSeriesIdentifier timeseriesID;
