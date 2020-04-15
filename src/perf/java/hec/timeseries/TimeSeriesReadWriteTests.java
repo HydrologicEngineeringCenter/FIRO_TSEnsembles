c8cd03f6efa88bd09f907048ec493f573f2ab19d
Mike Neilson <michael.a.neilson@usace.army.mil>
2020-04-15 07:04:32 -0700

    added mechanism to run performance tests.

---
+++ src/perf/java/hec/timeseries/TimeSeriesReadWriteTests.java
@@ -0,0 +1,22 @@
+package hec.timeseries;
+
+import hec.ensemble.TestingPaths;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.List;
+
+import hec.*;
+import hec.TestFixtures;
+
+
+public class TimeSeriesReadWriteTests {
+
+    @Test
+    public void measure_write_and_read_performance(){
+        fail("not yet implemented");
+    }
+    
+
+}
