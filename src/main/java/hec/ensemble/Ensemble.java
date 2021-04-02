7f73fcab5d103648f1467f261ebafee3208f7d94
Will Lehman <william.p.lehman@usace.army.mil>
2021-04-01 19:26:15 -0700

    adding a method to compute a value for each trace across all time fixes #52

--- src/main/java/hec/ensemble/Ensemble.java
+++ src/main/java/hec/ensemble/Ensemble.java
@@ -2,6 +2,7 @@ package hec.ensemble;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import hec.stats.Computable;
 
 /**
  * an Ensemble is an array of time-series data
@@ -74,5 +75,12 @@ public class Ensemble
     public float[][] getValues() {
       return values;
     }
-
+    public float[] iterateForTracesAcrossTime(Computable cmd){
+      int size= values.length;
+      float[] rval = new float[size];
+      for (int i = 0; i <size ; i++) {
+          rval[i] = cmd.compute(values[i]);
+      }
+      return rval;
+    }
   }
