2f6dd95db9529c7ec06a21665296d4e396d5feee
dvdhoz <JESUS143you>
2021-04-26 16:28:59 -0700

    added integration test.  Mean Computable ensemble test

--- src/test/java/hec/stats/MeanComputableTest.java
+++ src/test/java/hec/stats/MeanComputableTest.java
@@ -1,6 +1,14 @@
 package hec.stats;
 
+
+import hec.JdbcTimeSeriesDatabase;
+import hec.TimeSeriesDatabase;
+import hec.ensemble.*;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,5 +26,29 @@ class MeanComputableTest {
         float[] num = {10,30,45,80,50};
         float results = test.compute(num);
         assertEquals(43, results);
+    }
+    @Test
+    public void testMeanWithEnsemble() {
+        try {
+            String fn = TestingPaths.instance.getTempDir()+"/importCsvToDatabase.db";
+            File f = new File(fn);
+            f.delete();
+
+            DatabaseGenerator.createTestDatabase(fn,1);
+            TimeSeriesDatabase db  =new JdbcTimeSeriesDatabase(fn, JdbcTimeSeriesDatabase.CREATION_MODE.OPEN_EXISTING_UPDATE);
+            // --- READ
+            TimeSeriesIdentifier tsid = new TimeSeriesIdentifier("Kanektok.SCRN2","flow");
+            EnsembleTimeSeries ets =  db.getEnsembleTimeSeries(tsid);
+            List<ZonedDateTime> issueDates = ets.getIssueDates();
+            Ensemble e = db.getEnsemble(tsid, issueDates.get(1));
+            Computable test = new MeanComputable();
+            float[] output = e.iterateForTimeAcrossTraces(test);
+            assertEquals(0.9668081998825073, output[3]);
+
+
+        } catch (Exception e) {
+            Logger.logError(e);
+            fail();
+        }
     }
 }
