fd6f39b0168c4c683e0ed973ba7271453428ced5
dvdhoz <JESUS143you>
2021-04-29 08:53:39 -0700

    added test to compute mean for ensemble across time and across traces

--- src/test/java/hec/stats/MeanComputableTest.java
+++ src/test/java/hec/stats/MeanComputableTest.java
@@ -14,41 +14,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MeanComputableTest {
     @Test
-    public void testMeanCompute() {
+    public void testMeanComputeSimpleArray() {
         MeanComputable test = new MeanComputable();
         float[] num = {1,2,3,4,5,6,7,8};
         float results = test.compute(num);
         assertEquals(4.5, results);
     }
     @Test
-    public void testMeanCompute2() {
+    public void testMeanComputeSimpleArrayTens() {
         MeanComputable test = new MeanComputable();
         float[] num = {10,30,45,80,50};
         float results = test.compute(num);
         assertEquals(43, results);
     }
     @Test
-    public void testMeanWithEnsemble() {
+    public void testMeanWithEnsembleTimeAcrossTraces() {
         try {
-            String fn = TestingPaths.instance.getTempDir()+"/importCsvToDatabase.db";
-            File f = new File(fn);
-            f.delete();
-
-            DatabaseGenerator.createTestDatabase(fn,1);
-            TimeSeriesDatabase db  =new JdbcTimeSeriesDatabase(fn, JdbcTimeSeriesDatabase.CREATION_MODE.OPEN_EXISTING_UPDATE);
-            // --- READ
-            TimeSeriesIdentifier tsid = new TimeSeriesIdentifier("Kanektok.SCRN2","flow");
-            EnsembleTimeSeries ets =  db.getEnsembleTimeSeries(tsid);
-            List<ZonedDateTime> issueDates = ets.getIssueDates();
-            Ensemble e = db.getEnsemble(tsid, issueDates.get(1));
+            Ensemble e = getEnsemble();
             Computable test = new MeanComputable();
             float[] output = e.iterateForTimeAcrossTraces(test);
             assertEquals(0.9668081998825073, output[3]);
-
-
         } catch (Exception e) {
             Logger.logError(e);
             fail();
         }
     }
+    @Test
+    public void testMeanWithEnsembleTracesAcrossTime() {
+        try {
+            Ensemble e = getEnsemble();
+            Computable test = new MeanComputable();
+            float[] output = e.iterateForTracesAcrossTime(test);
+            assertEquals(-10.0833740234375, output[3]);
+        } catch (Exception e) {
+            Logger.logError(e);
+            fail();
+        }
+    }
+
+    private Ensemble getEnsemble() throws Exception {
+        String fn = TestingPaths.instance.getTempDir() + "/importCsvToDatabase.db";
+        File f = new File(fn);
+        f.delete();
+
+        DatabaseGenerator.createTestDatabase(fn, 1);
+        TimeSeriesDatabase db = new JdbcTimeSeriesDatabase(fn, JdbcTimeSeriesDatabase.CREATION_MODE.OPEN_EXISTING_UPDATE);
+        // --- READ
+        TimeSeriesIdentifier tsid = new TimeSeriesIdentifier("Kanektok.SCRN2", "flow");
+        EnsembleTimeSeries ets = db.getEnsembleTimeSeries(tsid);
+        List<ZonedDateTime> issueDates = ets.getIssueDates();
+        return db.getEnsemble(tsid, issueDates.get(0));
+    }
+
 }
