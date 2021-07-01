28821f6927a0beefb95e863459dad65942fc8536
Sara-HEC <sara.oconnell@usace.army.mil>
2021-07-01 15:47:14 -0700

    removed unnecessary commented code

--- FIRO_TSEnsembles/src/main/java/hec/stats/MaxComputable.java
+++ FIRO_TSEnsembles/src/main/java/hec/stats/MaxComputable.java
@@ -3,16 +3,7 @@ package hec.stats;
 public class MaxComputable implements Computable{
     @Override
     public float compute(float[] values){
-        //calculate the min of values
-        /*float returnvalue = 99999999999.00f;
-        int size= values.length;
-        float[] rval = new float[size];
-        for (int i = 0; i <size ; i++) {
-            if(returnvalue>values[i]){
-                returnvalue = values[i];
-            }
-        }
-        return returnvalue;*/
+        //calculate the max of values
         int size= values.length;
         java.util.Arrays.sort(values);
         return values[size-1];
