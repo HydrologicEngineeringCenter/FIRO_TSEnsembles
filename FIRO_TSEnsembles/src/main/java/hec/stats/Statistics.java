7cdf49168df67fd3700660f19d60f4b823028967
David Ho <david.ho@usace.army.mil>
2022-04-28 15:03:32 -0700

    rebase to master

--- FIRO_TSEnsembles/src/main/java/hec/stats/Statistics.java
+++ FIRO_TSEnsembles/src/main/java/hec/stats/Statistics.java
@@ -14,14 +14,14 @@ public enum Statistics {
     MAXAVERAGEDURATION,
     MAXACCUMDURATION;
 
-    public static String pack(EnumSet<Statistics> set){
+    public static String pack(EnumSet<Statistics> set){ //returns a list of the statistics in a string format
         String ret = "";
         for (Statistics s : Statistics.values()){
             if (set.contains(s)){
                 ret += s.name() + ",";
             }
         }
-        ret = ret.substring(0,ret.length()-1);
+        ret = ret.substring(0,ret.length()-1); // removes the last comma
         return ret;
     }
 
