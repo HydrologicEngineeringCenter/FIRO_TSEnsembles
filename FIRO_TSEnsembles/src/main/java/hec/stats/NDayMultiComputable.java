47b364e8bd4e61dc88043d4b1ec1a06ad88e25c4
David Ho <david.ho@usace.army.mil>
2022-05-05 16:34:14 -0700

    adding javadocs to NDayMultiComputable and tests

--- FIRO_TSEnsembles/src/main/java/hec/stats/NDayMultiComputable.java
+++ FIRO_TSEnsembles/src/main/java/hec/stats/NDayMultiComputable.java
@@ -1,11 +1,15 @@
 package hec.stats;
 
 public class NDayMultiComputable implements Computable, StatisticsReportable, Configurable {
+    /**
+     * The n day multi computable computes a multiComputable stat (cumulative) and gets the cumulative value for the specified day.
+     * Intended to be used for iterating for traces across time
+     * @param numberDays this is expected to be in integer days
+     */
 
     private MultiComputable _stepOne;
     private int _day;
     Configuration _c;
-    private int _timesteps = 0;
 
     public NDayMultiComputable(MultiComputable stepOne, int numberDays) {
         _stepOne = stepOne;
@@ -16,7 +20,7 @@ public class NDayMultiComputable implements Computable, StatisticsReportable, Co
     public float compute(float[] values) {
         values = _stepOne.MultiCompute(values);
         int timestep = (int) _c.getDuration().toHours();
-        int timestepDay = timestep / 24;
+        int timestepDay = timestep * 24;
         return values[timestepDay * _day];
     }
 
