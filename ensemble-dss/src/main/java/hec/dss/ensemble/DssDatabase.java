8ab622c643ce7923169eff3006f84d0e5b2edbc3
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2022-05-19 14:27:01 -0700

    initial ability to write to DSS
    using hec-monolith for DSS I/O
    update firo-plugin to use hec-monolith

--- ensemble-dss/src/main/java/hec/dss/ensemble/DssDatabase.java
+++ ensemble-dss/src/main/java/hec/dss/ensemble/DssDatabase.java
@@ -6,43 +6,57 @@ import java.time.format.DateTimeFormatter;
 import hec.EnsembleDatabase;
 import hec.RecordIdentifier;
 import hec.ensemble.Ensemble;
+import hec.MetricDatabase;
 import hec.ensemble.EnsembleTimeSeries;
+
 import hec.heclib.dss.HecDss;
+import hec.heclib.dss.HecTimeSeries;
 import hec.io.TimeSeriesCollectionContainer;
 import hec.io.TimeSeriesContainer;
-
+import hec.heclib.util.HecTime;
 
 /**
  * DssDatabase implements EnsembleDatabase.
  * It is used to work with time-series of ensembles.
  * Data is stored in a DSS file, using the Collections and Version features of the DSS F-Part tag.
  */
-public class DssDatabase implements EnsembleDatabase{
+public class DssDatabase implements EnsembleDatabase,MetricDatabase {
     private String dssFileName;
     public DssDatabase(String dssFileName){
         this.dssFileName= dssFileName;
     }
 
-    private static String buildDssPath(RecordIdentifier recordID, java.time.ZonedDateTime issue_time){
-        // to do define translation
-        return "//"+recordID.location+"/"+recordID.parameter+"///"+buildFpart(issue_time);
+    private static String buildDssPath(int memberNumber,EnsembleTimeSeries ets, Ensemble e){
+        // translation to DSS Path
+        // TO DO :  E-Part (1Hour,1Day, etc..)
+
+       RecordIdentifier recordID = ets.getTimeSeriesIdentifier();
+       ZonedDateTime issue_time = e.getIssueDate();
+
+
+        return "//"+recordID.location+"/"+recordID.parameter+"/"
+                + "/1Hour/"+buildFpart(memberNumber,issue_time)+"/";
     }
 
     /**
      * Builds F part of DSS path
      * We are not including C:000001-- TimeSeriesCollectionContainer will do that
-     * @param version
+     *
+     * C - value is 6 alphanumeric characters
+     * T - value is valid date and time in the format YYYYMMDD-hhmm
+     * N - no validation performed
+     * V - value is valid date and time in the format YYYYMMDD-hhmmss
+     * R - value is even number of characters and each 2-character pair
+     *     must be either "--" or an upper or lower case alphabetic
+     *     character followed by a digit character (not restricted to '0')
+     * @param t - time stamp saved in T: tag in F part
+     * @param  member - ensemble member number to be in C: tag in F part
      * @return
      */
-    private static String buildFpart(java.time.ZonedDateTime version){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
-        return "V:"+formatter.format(version);
-    }
-    /* We may not need this version that includes member/collection C:000001
     private static String buildFpart(int member , java.time.ZonedDateTime t){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
-        return "C:"+String.format("C:%06d", member)+"|V:"+formatter.format(t);
-    }*/
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmm");
+        return String.format("C:%06d", member)+"|T:"+formatter.format(t);
+    }
 
     public Ensemble getEnsemble(RecordIdentifier recordID, ZonedDateTime issue_time){
         Ensemble rval = null; //new hec.ensemble.Ensemble();
@@ -65,11 +79,17 @@ public class DssDatabase implements EnsembleDatabase{
     }
     public void write(EnsembleTimeSeries ets) throws Exception{
         // get access to DSS
-        HecDss dss = HecDss.open(dssFileName);
+        HecTimeSeries dss = new hec.heclib.dss.HecTimeSeries(dssFileName);
         //translate EnsembleTimeSeries into TimeSeriesCollectionContainer
         TimeSeriesCollectionContainer containers = loadContainers(ets);
         // write TimeSeriesCollectionContainer to DSS file
-        dss.write(containers);
+        for (int i = 0; i < containers.size(); i++) {
+            TimeSeriesContainer tsc = containers.get(i);
+            hec.heclib.dss.HecDataManager.setMessageLevel(15);
+            dss.write(tsc);
+            break;
+        }
+
         // close resources
         dss.close();
         System.out.println("done.");
@@ -81,19 +101,37 @@ public class DssDatabase implements EnsembleDatabase{
             float[][] values = e.getValues();
             for (int row = 0; row <values.length ; row++) {
                 TimeSeriesContainer tsc = new TimeSeriesContainer();
-                tsc.values = convertFloatsToDoubles(values[row]);
-                tsc.setFullName(buildDssPath(ets.getTimeSeriesIdentifier(),e.getIssueDate()));
-                //tsc. other STUFF>>>
+                tsc.setValues(convertFloatsToDoubles(values[row]));
+                tsc.setFullName(buildDssPath((row+1),ets,e));
+                tsc.units = e.getUnits();
+                tsc.setType(ets.getDataType());
+                tsc.setStartTime(getHecStartTime(e));
                 rval.add(tsc);
+                break;
             }
+            break;
         }
         rval.finishedAdding();
         return rval;
     }
 
 
+    private static HecTime getHecStartTime(Ensemble e) {
+        String dateStr = "";
+        DateTimeFormatter dssDateFormat = DateTimeFormatter.ofPattern("ddMMMyyyy HHmm");
+        if (e.getTimeCount() > 0)
+            dateStr = e.startDateTime()[0].format(dssDateFormat);
+        HecTime rval = new HecTime(dateStr);
+        return rval;
+    }
 
-    //https://stackoverflow.com/questions/2019362/how-to-convert-array-of-floats-to-array-of-doubles-in-java
+    /**
+     * convert array of floats to array of doubles
+     * //https://stackoverflow.com/questions/2019362/how-to-convert-array-of-floats-to-array-of-doubles-in-java
+     * @param input
+     * @return
+     */
+
     private static double[] convertFloatsToDoubles(float[] input)
     {
         if (input == null)
@@ -121,5 +159,35 @@ public class DssDatabase implements EnsembleDatabase{
     @Override
     public void close() throws Exception {
 
+    }
+
+    @Override
+    public hec.metrics.MetricCollection getMetricCollection(hec.RecordIdentifier timeseriesID, java.time.ZonedDateTime issue_time) {
+        return null;
+    }
+
+    @Override
+    public hec.metrics.MetricCollectionTimeSeries getMetricCollectionTimeSeries(hec.RecordIdentifier timeseriesID) {
+        return null;
+    }
+
+    @Override
+    public java.util.List<java.time.ZonedDateTime> getMetricCollectionIssueDates(hec.RecordIdentifier timeseriesID) {
+        return null;
+    }
+
+    @Override
+    public void write(hec.metrics.MetricCollectionTimeSeries[] metricsArray) throws Exception {
+
+    }
+
+    @Override
+    public void write(hec.metrics.MetricCollectionTimeSeries metrics) throws Exception {
+
+    }
+
+    @Override
+    public java.util.List<hec.RecordIdentifier> getMetricTimeSeriesIDs() {
+        return null;
     }
 }
