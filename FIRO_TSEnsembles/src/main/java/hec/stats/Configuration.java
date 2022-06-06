c664d3a2a0d5d051ef4c88fb7611c2b730cf43c3
bbeam <Brennan.B.Beam@usace.army.mil>
2022-06-06 13:40:04 -0700

    add to XML to Configuration

--- FIRO_TSEnsembles/src/main/java/hec/stats/Configuration.java
+++ FIRO_TSEnsembles/src/main/java/hec/stats/Configuration.java
@@ -1,10 +1,52 @@
 package hec.stats;
+import org.jdom.Attribute;
+import org.jdom.Element;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 public interface Configuration{
     public Duration getDuration();
     public ZonedDateTime getIssueDate();
     public ZonedDateTime getStartDate();
     public String getUnits();
+
+    default org.jdom.Element toXML(){
+        Field[] fields = this.getClass().getDeclaredFields();
+        Element ele = new Element(this.getClass().getName());
+        for(Field f: fields){
+            try {
+                Type type = f.getType();
+                String stringType = type.getTypeName();
+                String fieldName = f.getName();
+                Object objectFieldValue = f.get(this);
+                String attribute = null;
+
+                switch(stringType){
+                    case "java.time.Duration":
+                        Duration durationVal = (Duration) objectFieldValue;
+                        attribute = durationVal.toString();
+                        break;
+                    case "java.time.ZonedDateTime":
+                        ZonedDateTime zonedDateTimeVal = (ZonedDateTime) objectFieldValue;
+                        attribute = zonedDateTimeVal.toString();
+                        break;
+                    case "java.lang.String":
+                        attribute = (String) objectFieldValue;
+                        break;
+                }
+                ele.setAttribute(fieldName,attribute);
+
+            } catch (IllegalArgumentException | IllegalAccessException ex) {
+                System.out.println("Failed Cast" );
+            }
+        }
+        return ele;
+    }
 }
