96364c80e9a764df823a25afb0fed582ccfdb8bd
Brennan Beam <BrennanBeam@gmail.com>
2022-06-09 16:10:40 -0700

    Clean up computable test

--- FIRO_TSEnsembles/src/test/java/hec/stats/ComputableTest.java
+++ FIRO_TSEnsembles/src/test/java/hec/stats/ComputableTest.java
@@ -2,63 +2,43 @@ package hec.stats;
 
 import hec.ensemble.Ensemble;
 import hec.ensemble.TestData;
-import org.jdom.Element;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComputableTest {
-    Ensemble e = TestData.getSampleEnsemble();
-    Computable maxTest = new MaxComputable();
     Computable maxAccumDuration = new MaxAccumDuration(3);
     Computable maxAvgDuration = new MaxAvgDuration(3);
     Computable maxComputable = new MaxComputable();
     Computable meanComputable = new MeanComputable();
     Computable medianComputable = new MedianComputable();
     Computable minComputable = new MinComputable();
-    Computable percientiles = new PercentilesComputable((float) .01);
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
+    Computable percentiles = new PercentilesComputable((float) .01);
 
     @Test
-    void toXML() {
+    void ToAndFromXML() {
         try {
-            var x = maxTest.toXML();
-            x = maxAccumDuration.toXML();
-            x = maxAvgDuration.toXML();
-            x = maxComputable.toXML();
-            x = meanComputable.toXML();
-            x = medianComputable.toXML();
-            x = minComputable.toXML();
-            x = percientiles.toXML();
+            var a= Serializer.toXML(maxAccumDuration);
+            var b = Serializer.toXML( maxAvgDuration);
+            var c = Serializer.toXML( maxComputable);
+            var d = Serializer.toXML(meanComputable);
+            var e = Serializer.toXML(medianComputable);
+            var f = Serializer.toXML(minComputable);
+            var g = Serializer.toXML(percentiles);
+
+            var A = Serializer.fromXML(a);
+            var B = Serializer.fromXML(b);
+            var C = Serializer.fromXML(c);
+            var D = Serializer.fromXML(d);
+            var E = Serializer.fromXML(e);
+            var F = Serializer.fromXML(f);
+            var G = Serializer.fromXML(g);
+
         } catch (Exception exception) {
             exception.printStackTrace();
             fail();
         }
     }
 
-    @Test
-    void fromXML() {
-        try {
-            var x = Computable.fromXML(maxTest.toXML());
-            x= Computable.fromXML(maxAccumDuration.toXML());
-            x = Computable.fromXML(maxAvgDuration.toXML());
-            x = Computable.fromXML(maxComputable.toXML());
-            x = Computable.fromXML(meanComputable.toXML());
-            x = Computable.fromXML(medianComputable.toXML());
-            x = Computable.fromXML(medianComputable.toXML());
-            x = Computable.fromXML(minComputable.toXML());
-            x = Computable.fromXML(percientiles.toXML());
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            fail();
-        }
-    }
 }
