7b1e893acb44ab8c0be6e861c053fd06423b390f
Daniel Hamill <hamill.daniel@gmail.com>
2023-07-21 13:12:33 -0700

    Update Version to 20230718 (#175)
    Need to update version table in database before updating index to
    three unique identifiers.
    
    Addresses #174

--- FIRO_TSEnsembles/src/main/resources/update_20210922_to_20230718.sql
+++ FIRO_TSEnsembles/src/main/resources/update_20210922_to_20230718.sql
@@ -1,2 +1,4 @@
+UPDATE version SET version = '20230718';
+
 DROP INDEX if EXISTS idx_u_ensemble_timeseries;
 CREATE UNIQUE INDEX if not EXISTS idx_u_ensemble_timeseries ON ensemble_timeseries(location,parameter_name,version);
