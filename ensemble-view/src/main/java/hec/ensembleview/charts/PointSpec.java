9e653fb9f74aa99522cc87e5b9f18468f64aa453
David Ho <david.ho@usace.army.mil>
2023-07-25 12:54:32 -0700

    Adding method for including probability plot
    Adding method to add probability data type for EnsembleArray compute type.

--- ensemble-view/src/main/java/hec/ensembleview/charts/PointSpec.java
+++ ensemble-view/src/main/java/hec/ensembleview/charts/PointSpec.java
@@ -1,6 +1,7 @@
 package hec.ensembleview.charts;
 
 import java.awt.*;
+import java.util.Map;
 
 public class PointSpec {
     int rangeAxis;
@@ -14,6 +15,15 @@ public class PointSpec {
                      Color pointColor, String pointName) {
         this.rangeAxis = rangeAxis;
         this.yValue = y;
+        this.lineStroke = lineStroke;
+        this.pointColor = pointColor;
+        this.pointName = pointName;
+    }
+
+    public PointSpec(int rangeAxis, Map<Float, Float> prob, Stroke lineStroke,
+                     Color pointColor, String pointName) {
+        this.rangeAxis = rangeAxis;
+        this.prob = prob;
         this.lineStroke = lineStroke;
         this.pointColor = pointColor;
         this.pointName = pointName;
