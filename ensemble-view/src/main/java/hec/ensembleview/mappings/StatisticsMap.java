7e74d4d75ad24a2c22e2cc359417f4483c2f3ec6
David Ho <david.ho@usace.army.mil>
2023-08-30 10:14:05 -0700

    Initiate actionListener when there are statistics checked.

--- ensemble-view/src/main/java/hec/ensembleview/mappings/StatisticsMap.java
+++ ensemble-view/src/main/java/hec/ensembleview/mappings/StatisticsMap.java
@@ -19,11 +19,16 @@ public class StatisticsMap { //maps the statistics to the specific statistics pa
     }
 
     public void setTimeStatisticsMap() {
-        this.timeSeriesMapList = getTimeSeriesMapList();
+        if(this.timeSeriesMapList.size() == 0) {
+            return;
+        }
         support.firePropertyChange("time", false, true);
     }
 
     public void setEnsembleStatisticsMap() {
+        if(this.ensembleSeriesMapList.size() == 0) {
+            return;
+        }
         support.firePropertyChange("ensemble", false, true);
     }
 
