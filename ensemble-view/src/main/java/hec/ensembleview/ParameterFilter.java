bc14551153f807792fbad81acfe7419523373380
David Ho <david.ho@usace.army.mil>
2024-01-22 14:15:36 -0800

    Adding sediment parameter types to ensemble viewer.
    Fixes #239

--- ensemble-view/src/main/java/hec/ensembleview/ParameterFilter.java
+++ ensemble-view/src/main/java/hec/ensembleview/ParameterFilter.java
@@ -4,6 +4,7 @@ import hec.RecordIdentifier;
 
 public class ParameterFilter {
     public static RecordIdentifier checkParameter(RecordIdentifier recordIdentifier) {
+
         String parameter = recordIdentifier.parameter;
         switch (parameter.toUpperCase()) {
             case "FLOW":
@@ -14,9 +15,26 @@ public class ParameterFilter {
             case "TEMPERATURE-AIR":
             case "BULK-FLOW-COMBINE":
             case "SEDIMENT LOAD-COMBINE":
+            case "SEDIMENT LOAD-COMBINE-CLAY":
+            case "SEDIMENT LOAD-COMBINE-SAND":
+            case "SEDIMENT LOAD-COMBINE-SILT":
+            case "SEDIMENT LOAD-COMBINE-GRAVEL":
             case "SEDIMENT LOAD-OUT":
-            case "SEDIMENT VOLUME-OUT":
-            case "BULK FLOW-OUT":
+            case "SEDIMENT LOAD-CLAY":
+            case "SEDIMENT LOAD-SAND":
+            case "SEDIMENT LOAD-SILT":
+            case "SEDIMENT LOAD-GRAVEL":
+            case "SEDIMENT VOLUME-COMBINE":
+            case "SEDIMENT VOLUME-COMBINE-CLAY":
+            case "SEDIMENT VOLUME-COMBINE-SAND":
+            case "SEDIMENT VOLUME-COMBINE-SILT":
+            case "SEDIMENT VOLUME-COMBINE-GRAVEL":
+            case "SEDIMENT VOLUME":
+            case "SEDIMENT VOLUME-CLAY":
+            case "SEDIMENT VOLUME-SAND":
+            case "SEDIMENT VOLUME-SILT":
+            case "SEDIMENT VOLUME-GRAVEL":
+            case "BULK FLOW":
             case "BULK FLOW-COMBINE":
                 return recordIdentifier;
             default:
