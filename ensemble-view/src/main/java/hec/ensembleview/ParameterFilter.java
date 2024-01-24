910e80f06a43c2cb9b818f7cfcd01833fd1f8e81
David Ho <david.ho@usace.army.mil>
2024-01-24 10:42:36 -0800

    Update ParameterFilter to include Sediment outputs

--- ensemble-view/src/main/java/hec/ensembleview/ParameterFilter.java
+++ ensemble-view/src/main/java/hec/ensembleview/ParameterFilter.java
@@ -24,17 +24,27 @@ public class ParameterFilter {
             case "SEDIMENT LOAD-SAND":
             case "SEDIMENT LOAD-SILT":
             case "SEDIMENT LOAD-GRAVEL":
+            case "SEDIMENT LOAD-OUT-CLAY":
+            case "SEDIMENT LOAD-OUT-SAND":
+            case "SEDIMENT LOAD-OUT-SILT":
+            case "SEDIMENT LOAD-OUT-GRAVEL":
             case "SEDIMENT VOLUME-COMBINE":
             case "SEDIMENT VOLUME-COMBINE-CLAY":
             case "SEDIMENT VOLUME-COMBINE-SAND":
             case "SEDIMENT VOLUME-COMBINE-SILT":
             case "SEDIMENT VOLUME-COMBINE-GRAVEL":
             case "SEDIMENT VOLUME":
+            case "SEDIMENT VOLUME-OUT":
             case "SEDIMENT VOLUME-CLAY":
             case "SEDIMENT VOLUME-SAND":
             case "SEDIMENT VOLUME-SILT":
             case "SEDIMENT VOLUME-GRAVEL":
+            case "SEDIMENT VOLUME-OUT-CLAY":
+            case "SEDIMENT VOLUME-OUT-SAND":
+            case "SEDIMENT VOLUME-OUT-SILT":
+            case "SEDIMENT VOLUME-OUT-GRAVEL":
             case "BULK FLOW":
+            case "BULK FLOW-OUT":
             case "BULK FLOW-COMBINE":
                 return recordIdentifier;
             default:
